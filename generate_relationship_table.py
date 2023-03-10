a32a3140d894d1a6f3d592d35094af5d4ff8c3b9
HenryMiyawaki <miyawakihenry@gmail.com>
2023-03-10 16:57:31 -0300

    creating generation logic

--- generate_relationship_table.py
+++ generate_relationship_table.py
@@ -7,12 +7,14 @@ KEYWORDS = {
     "station_name": "Estação"
 }
 
+
 class RelationshipTable():
 
     __dataset = None
     __columns = None
     __keywords = None
-    def __init__(self, dataset, columns = None, keyword = None):
+
+    def __init__(self, dataset, columns=None, keyword=None):
         self.__dataset = dataset
 
         if keyword == None:
@@ -22,7 +24,7 @@ class RelationshipTable():
             self.columns = self.get_columns()
 
     def get_columns(self):
-        
+
         if self.__columns != None:
             return self.__columns
 
@@ -33,11 +35,40 @@ class RelationshipTable():
 
         return cols
 
+    def verifyIntegration(self, stationRow, stationCol, position):
+        if stationRow == stationCol:
+            count = 0
+            for line in self.__dataset:
+                for station in line[self.__keywords["station"]]:
+                    count = count + 1
+                    if station[self.__keywords["station_name"]] == stationRow and count != position:
+                        return 1
+        else:
+            for line in self.__dataset:
+                nextStation = False
+                for station in line[self.__keywords["station"]]:
+                    if nextStation and station[self.__keywords["station_name"]] == stationCol:
+                        print(stationRow + " conecta com " + stationCol)
+                        return 1
+                    else:
+                        nextStation = False
+
+                    if station[self.__keywords["station_name"]] == stationRow:
+                        nextStation = True
+        return 0
 
 
 table = RelationshipTable(data.SUBWAY_DATASET)
 
-count = 1
-for station in table.get_columns():
-    print(str(count) + " " + station)
-    count += 1
+count = 0
+matrix = []
+
+for row in table.get_columns():
+    aux = []
+    count = count + 1
+    for col in table.get_columns():
+        aux.append(table.verifyIntegration(row, col, count))
+
+    matrix.append(aux)
+
+print(matrix)
