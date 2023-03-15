2d8824d0ca0bf706017d09b1a7909287d7b1870e
Felipe N <Felipe.Melo@ibm.com>
2023-03-15 20:00:46 -0300

    fixed name

--- generate_relationship_table.py
+++ generate_relationship_table.py
@@ -1,5 +1,9 @@
 
+from os import stat
+import sys
+import pandas as pd
 from dataset import data
+
 
 KEYWORDS = {
     "line": "Linha",
@@ -19,7 +23,7 @@ class RelationshipTable():
             self.__keywords = KEYWORDS
 
         if columns == None:
-            self.columns = self.get_columns()
+            self.__columns = self.get_columns()
 
     def get_columns(self):
         
@@ -33,11 +37,88 @@ class RelationshipTable():
 
         return cols
 
+    def evaluate_matrix(self):
+
+        mx_cols = self.__columns
+        mx_lines = self.__columns
+
+        last_station = None
+        next_station = None
+        mx = []
+
+        if (mx_cols == None) or (mx_lines == None):
+            cols = self.get_columns()
+            lines = cols
+
+        for i,c in enumerate(mx_cols):
+            mx.append([])
+            for l in mx_lines:
+                mx[i].append(0)
+
+
+        for idx_col, col in enumerate(mx_cols):
+            for idx_lines, line in enumerate(mx_lines):
+    
+                station_line = mx_cols[idx_col]
+                station_col = mx_lines[idx_lines]
+              
+                if (station_line == station_col):
+                    
+                    mx[idx_col][idx_lines] = 1
+
+                    if (len(mx[idx_col]) != idx_lines + 1):
+                        mx[idx_col][idx_lines + 1] = 1
+                    if (idx_lines != 0):
+                        mx[idx_col][idx_lines - 1] = 1
+
+            
+        
+        return mx
+
+    def transform_dataframe(self):
+        mx = self.evaluate_matrix()
+        data = {}
+
+        for i, columns in enumerate(mx):
+            data[self.__columns[i]] = columns
+
+        df = pd.DataFrame(data, index=self.__columns)
+        return df
+
+    def tranform_csv():
+        pass
 
 
 table = RelationshipTable(data.SUBWAY_DATASET)
+cols = table.get_columns()
+df = table.transform_dataframe()
+df.to_excel("./relationship.xlsx")
 
-count = 1
-for station in table.get_columns():
-    print(str(count) + " " + station)
-    count += 1
+_cols = []
+
+for c in cols:
+    c = c.replace("Estação", "Estacao") 
+    c = c.replace("çã", "ca")
+    c = c.replace("ã", "a")
+    c = c.replace("ç", "c")
+    c = c.replace("é", "e")
+    c = c.replace("ê", "e")
+    c = c.replace("É", "e")
+    c = c.replace("í", "i")
+    c = c.replace("á", "a")
+    c = c.replace("Á", "A")
+    c = c.replace("ó", "o")
+    c = c.replace("Ó", "O")
+    c = c.replace("ô", "o")
+    c = c.replace("ú", "u")
+    c = c.replace("Ú", "U")
+    c = c.replace("-", "")
+
+    _cols.append(c)
+    
+
+print(_cols)
+mx = table.evaluate_matrix()
+
+for i, item in  enumerate(mx):
+    print(f"{_cols[i]}," + str(item)[1:len(str(item)) - 1])
