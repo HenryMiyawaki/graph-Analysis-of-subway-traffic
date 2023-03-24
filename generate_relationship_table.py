4946abbf8cd9df60288412025cf8bb52c8d1f264
Felipe N <Felipe.Melo@ibm.com>
2023-03-23 23:48:07 -0300

    changed to fix double col and row on adjascent matrix

--- generate_relationship_table.py
+++ generate_relationship_table.py
@@ -1,124 +1,25 @@
 
-from os import stat
-import sys
-import pandas as pd
+from datetime import datetime
 from dataset import data
-
-
-KEYWORDS = {
-    "line": "Linha",
-    "station": "Estacoes",
-    "station_name": "Estação"
-}
-
-class RelationshipTable():
-
-    __dataset = None
-    __columns = None
-    __keywords = None
-    def __init__(self, dataset, columns = None, keyword = None):
-        self.__dataset = dataset
-
-        if keyword == None:
-            self.__keywords = KEYWORDS
-
-        if columns == None:
-            self.__columns = self.get_columns()
-
-    def get_columns(self):
-        
-        if self.__columns != None:
-            return self.__columns
-
-        cols = []
-        for line in self.__dataset:
-            for station in line[self.__keywords["station"]]:
-                cols.append(station[self.__keywords["station_name"]])
-
-        return cols
-
-    def evaluate_matrix(self):
-
-        mx_cols = self.__columns
-        mx_lines = self.__columns
-
-        last_station = None
-        next_station = None
-        mx = []
-
-        if (mx_cols == None) or (mx_lines == None):
-            cols = self.get_columns()
-            lines = cols
-
-        for i,c in enumerate(mx_cols):
-            mx.append([])
-            for l in mx_lines:
-                mx[i].append(0)
-
-
-        for idx_col, col in enumerate(mx_cols):
-            for idx_lines, line in enumerate(mx_lines):
-    
-                station_line = mx_cols[idx_col]
-                station_col = mx_lines[idx_lines]
-              
-                if (station_line == station_col):
-                    
-                    mx[idx_col][idx_lines] = 1
-
-                    if (len(mx[idx_col]) != idx_lines + 1):
-                        mx[idx_col][idx_lines + 1] = 1
-                    if (idx_lines != 0):
-                        mx[idx_col][idx_lines - 1] = 1
-
-            
-        
-        return mx
-
-    def transform_dataframe(self):
-        mx = self.evaluate_matrix()
-        data = {}
-
-        for i, columns in enumerate(mx):
-            data[self.__columns[i]] = columns
-
-        df = pd.DataFrame(data, index=self.__columns)
-        return df
-
-    def tranform_csv():
-        pass
+from openpyxl import Workbook
+from logic.RelatioshipTable import RelationshipTable
+from openpyxl import Workbook
 
 
 table = RelationshipTable(data.SUBWAY_DATASET)
-cols = table.get_columns()
-df = table.transform_dataframe()
-df.to_excel("./relationship.xlsx")
+table = table.evaluate_matrix()
+table = table.merge_duplicated()
 
-_cols = []
+relation_text = table.transform_text(table.get_matrix(), top_columns=True)
+filename = f'{datetime.now().strftime("%d_%m_%Y_%s")}_relationship_table.csv'
 
-for c in cols:
-    c = c.replace("Estação", "Estacao") 
-    c = c.replace("çã", "ca")
-    c = c.replace("ã", "a")
-    c = c.replace("ç", "c")
-    c = c.replace("é", "e")
-    c = c.replace("ê", "e")
-    c = c.replace("É", "e")
-    c = c.replace("í", "i")
-    c = c.replace("á", "a")
-    c = c.replace("Á", "A")
-    c = c.replace("ó", "o")
-    c = c.replace("Ó", "O")
-    c = c.replace("ô", "o")
-    c = c.replace("ú", "u")
-    c = c.replace("Ú", "U")
-    c = c.replace("-", "")
+wb = Workbook()
+ws = wb.active
 
-    _cols.append(c)
-    
+for i, line in enumerate(relation_text.splitlines()):
+    columns = line.split(',')
+    for j, column in enumerate(columns):
+        ws.cell(row=i+1, column=j+1, value=column)
 
-print(_cols)
-mx = table.evaluate_matrix()
-
-for i, item in  enumerate(mx):
-    print(f"{_cols[i]}," + str(item)[1:len(str(item)) - 1])
+wb.save(f"./output/{filename}.xlsx")
+print("---- output generated on ./output directory")
---
+++ logic/RelatioshipTable.py
@@ -0,0 +1,142 @@
+
+
+KEYWORDS = {
+    "line": "Linha",
+    "station": "Estacoes",
+    "station_name": "Estação"
+}
+
+
+class RelationshipTable():
+    
+    __dataset = None
+    __columns = None
+    __keywords = None
+    __matrix = None
+
+    def __init__(self, dataset, columns = None, keyword = None):
+        self.__dataset = dataset
+
+        if keyword == None:
+            self.__keywords = KEYWORDS
+
+        if columns == None:
+            self.__columns = self.get_columns()
+
+    def get_columns(self):
+        
+        if self.__columns != None:
+            return self.__columns
+
+        cols = []
+        for line in self.__dataset:
+            for station in line[self.__keywords["station"]]:
+                cols.append(station[self.__keywords["station_name"]])
+
+        return cols
+
+    def get_matrix(self):
+        if self.__matrix == None:
+            self.__matrix = self.evaluate_matrix()
+        
+        return self.__matrix
+
+    def evaluate_matrix(self):
+
+        mx_cols = self.__columns
+        mx_lines = self.__columns
+
+        if (mx_cols == None) or (mx_lines == None):
+            cols = self.get_columns()
+            lines = cols
+
+        mx = RelationshipTable.generate_matrix_object(len(mx_cols), len(mx_lines))
+
+        for idx_col, col in enumerate(mx_cols):
+            for idx_lines, line in enumerate(mx_lines):
+    
+                station_line = mx_cols[idx_col]
+                station_col = mx_lines[idx_lines]
+              
+                if (station_line == station_col):
+                    
+                    mx[idx_col][idx_lines] = 1
+
+                    if (len(mx[idx_col]) != idx_lines + 1):
+                        mx[idx_col][idx_lines + 1] = 1
+                        mx[idx_col + 1][idx_lines] = 1
+                    if (idx_lines != 0):
+                        mx[idx_col][idx_lines - 1] = 1
+                        mx[idx_col - 1][idx_lines] = 1
+
+        self.__matrix = mx
+        return self
+
+    def get_repeated_columns_index(self) -> list:
+
+        itens = {}
+
+        for index, string in enumerate(self.__columns):
+            if string not in list(itens.keys()):
+                itens[string] = []
+            itens[string].append(index)
+
+        for index, key in enumerate(list(itens.keys())):
+            if len(itens[key]) == 1:
+                del itens[key]
+
+        return itens 
+
+    def merge_duplicated(self):
+
+        repeated = self.get_repeated_columns_index()
+        self.merge_duplicated_cols(repeated_index = repeated)
+
+        return self
+    
+    def merge_duplicated_cols(self, repeated_index = None):
+        
+        if repeated_index == None:
+            repeated_index = self.get_repeated_columns_index()
+
+        print(repeated_index)
+
+        pass
+
+    def get_matrix(self):
+        if self.__matrix == None:
+            self.evaluate_matrix()
+        return self.__matrix
+
+    def transform_text(self, mx: list, top_columns: False) -> str:
+
+        cols = self.__columns
+        text = ""
+
+        if (cols is None) or len(cols) == 0:
+            return text
+
+        for i, c in enumerate(cols):
+            
+            mx_line = ",".join( map(lambda x: str(x), mx[i]) )
+            line = f'{c},{mx_line}\n'
+            text += line
+            
+        if top_columns:
+            columns = ",".join(cols)
+            text = f",{columns}\n{text}"
+            
+        return text
+
+
+    @staticmethod
+    def generate_matrix_object(lines: int, cols: int, format = 0) -> list:
+        mx = []
+        for i in range(lines):
+            mx.append([])
+            for l in range(cols):
+                mx[i].append(0)
+
+        return mx
+        
+        
