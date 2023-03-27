ffe3437068588eadee7eb540190a423075c6a11b
Felipemelo09 <felipe.melonunes09@gmail.com>
2023-03-27 01:04:30 -0300

    add dynamic table generation by sys args parameters

--- generate_plot_station.py
+++ generate_plot_station.py
@@ -1,17 +1,22 @@
 
+from datetime import datetime
 from dataset import data
 from logic.RelatioshipTable import RelationshipTable
 from igraph import Graph, plot
 
+import sys
+
 table = RelationshipTable(data.SUBWAY_DATASET)
 table = table.evaluate_matrix()
-table = table.merge_duplicated()
-table = table.turnoff_interception()
 
+for arg in sys.argv[1:]:
+    func = getattr(table, arg)
+    if func != None:
+        table = func()
 
 mx = table.get_matrix()
 
 g = Graph.Incidence(mx)
 g.vs["label"] = table.get_columns()
 
-plot(g, target='./myfile.pdf', bbox=(8000, 8000))
+plot(g, target=f'{datetime.now().strftime("%d_%m_%Y_%S")}_graph.pdf', bbox=(8000, 8000))
--- generate_relationship_table.py
+++ generate_relationship_table.py
@@ -1,25 +1,29 @@
-
 from datetime import datetime
 from dataset import data
 from openpyxl import Workbook
 from logic.RelatioshipTable import RelationshipTable
 from openpyxl import Workbook
-
+import sys
 
 table = RelationshipTable(data.SUBWAY_DATASET)
 table = table.evaluate_matrix()
-table = table.merge_duplicated()
+
+for arg in sys.argv[1:]:
+    func = getattr(table, arg)
+    if func != None:
+        table = func()
 
 relation_text = table.transform_text(table.get_matrix(), top_columns=True)
-filename = f'{datetime.now().strftime("%d_%m_%Y_%s")}_relationship_table.csv'
+filename = f'{datetime.now().strftime("%d_%m_%Y")}_{datetime.now().microsecond}_relationship_table.csv'
 
 wb = Workbook()
 ws = wb.active
+
 
 for i, line in enumerate(relation_text.splitlines()):
     columns = line.split(',')
     for j, column in enumerate(columns):
         ws.cell(row=i+1, column=j+1, value=column)
 
-wb.save(f"./output/{filename}.xlsx")
-print("---- output generated on ./output directory")
+wb.save(f"./data/output/adjascenty_matrix/{filename}.xlsx")
+print("---- output generated on ./data/output/adjascenty_matrix/ directory")
